143f233f02f533a75fde29ff212690d690e095f1
Danil Mikhaylov <damikh13@gmail.com>
2024-12-02 00:08:04 +0300

    + shadCN components (Button, Input)

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
+import { revalidatePath } from "next/cache";
 import { database } from "./db/database";
 import { bids as bids_schema } from "./db/schema";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 export default async function HomePage() {
     const bids = await database.query.bids.findMany(); // fetch all the bids from 'au_bids' table
 
-    return <main className="">
+    return <main className="container mx-auto py-12">
+        {
+            // mx-auto for ctrl +- (zomming in and out) positioning
+            // py-12 for padding for top and bottom
+        }
         <form action={async (formData: FormData) => {
             "use server";
             await database.insert(bids_schema).values({});
         }}>
-            <input name="bid" placeholder="Bid" />
-            <button type="submit">Place Bid</button>
+            <Input name="bid" placeholder="Bid" />
+            <Button type="submit">Place Bid</Button>
         </form>
 
         {
