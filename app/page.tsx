0662aeca99731cbf79595e4f4a4bdaf7b19ff10e
Danil Mikhaylov <damikh13@gmail.com>
2024-12-01 23:35:07 +0300

    displaying all existing bids when page loads

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
 import { database } from "./db/database";
-import { bids } from "./db/schema";
+import { bids as bids_schema } from "./db/schema";
 
-export default function HomePage() {
+export default async function HomePage() {
+    const bids = await database.query.bids.findMany(); // fetch all the bids from 'au_bids' table
+
     return <main className="">
         <form action={async (formData: FormData) => {
             "use server";
-            await database.insert(bids).values({});
+            await database.insert(bids_schema).values({});
         }}>
             <input name="bid" placeholder="Bid" />
             <button type="submit">Place Bid</button>
         </form>
+
+        {
+            bids.map(
+                (bid) => (
+                    <div key={bid.id}>{bid.id}</div>
+                )
+            )
+        }
     </main>;
 }
