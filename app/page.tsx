765908b159bffa6c410863510034f73ea1b2b9f9
Danil Mikhaylov <damikh13@gmail.com>
2024-12-04 00:22:46 +0300

    bringing google auth with auth.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import { revalidatePath } from "next/cache";
 import { database } from "./db/database";
 import { bids as bids_schema } from "./db/schema";
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import SignIn from "@/components/sign-in";
+import { revalidatePath } from "next/cache";
+import { SignOut } from "@/components/signout-button";
 
 export default async function HomePage() {
     const bids = await database.query.bids.findMany(); // fetch all the bids from 'au_bids' table
@@ -12,6 +14,9 @@ export default async function HomePage() {
             // mx-auto for ctrl +- (zomming in and out) positioning
             // py-12 for padding for top and bottom
         }
+
+        <SignIn/>
+
         <form action={async (formData: FormData) => {
             "use server";
             await database.insert(bids_schema).values({});
