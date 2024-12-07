7b12d1b2f193f5a927e61d3561f6b64298f61231
Danil Mikhaylov <damikh13@gmail.com>
2024-12-07 19:45:21 +0300

    added 'au_' prefix to DB table names (better for migration)

--- auth.ts
+++ auth.ts
@@ -2,8 +2,14 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { database } from "./app/db/database";
+import { accounts, sessions, users, verificationTokens } from "./app/db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: DrizzleAdapter(database),
+    adapter: DrizzleAdapter(database, {
+        usersTable: users,
+        accountsTable: accounts,
+        sessionsTable: sessions,
+        verificationTokensTable: verificationTokens,
+    }),
     providers: [Google],
 });
