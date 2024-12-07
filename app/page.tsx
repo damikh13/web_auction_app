3c6dc2d1d125a7650e391343067a7cc024f41c7c
Danil Mikhaylov <damikh13@gmail.com>
2024-12-07 23:43:37 +0300

    basic page cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { database } from "./db/database";
 import { bids as bids_schema, items } from "./db/schema";
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import SignIn from "@/components/sign-in";
 import { revalidatePath } from "next/cache";
 import { SignOut } from "@/components/signout-button";
@@ -16,39 +16,45 @@ export default async function HomePage() {
     const user = session.user;
     if (!user) return null;
 
-    return <main className="container mx-auto py-12">
-        {
-            // mx-auto for ctrl +- (zomming in and out) positioning
-            // py-12 for padding for top and bottom
-        }
+    return (
+        <main className="container mx-auto py-12 space-y-4">
+            {
+                // mx-auto for ctrl +- (zomming in and out) positioning
+                // py-12 for padding for top and bottom
+            }
 
-        {
-            session?.user?.name
-        }
+            <h1 className="text-4xl font-bold mb-8">post an item to sell</h1>
 
-        {
-            session ? <SignOut/> : <SignIn/>
-        }
+            <form
+                className="flex flex-col border p-8 rounded-xl space-y-4 max-w-lg"
+                action={async (formData: FormData) => {
+                    "use server";
+                    await database.insert(items).values({
+                        name: formData.get("name") as string,
+                        userId: session?.user?.id!,
+                    });
+                    revalidatePath("/"); // re-run whole component
+                }}
+            >
+                <Input
+                    className="max-w-lg"
+                    name="name"
+                    placeholder="enter item name:"
+                />
+                <Button className="self-end" type="submit">
+                    post item
+                </Button>
+            </form>
 
+            <h2 className="text-2xl font-bold">items for sale</h2>
 
-        <form action={async (formData: FormData) => {
-            "use server";
-            await database.insert(items).values({
-                name: formData.get("name") as string,
-                userId: session?.user?.id!,
-            });
-            revalidatePath("/"); // re-run whole component
-        }}>
-            <Input name="name" placeholder="enter item name:" />
-            <Button type="submit">post item</Button>
-        </form>
-
-        {
-            all_items.map(
-                (item) => (
-                    <div key={item.id}>{item.name}</div>
-                )
-            )
-        }
-    </main>;
+            <div className="grid grid-cols-4 gap-4">
+                {all_items.map((item) => (
+                    <div key={item.id} className="border p-8 rounded-xl">
+                        {item.name}
+                    </div>
+                ))}
+            </div>
+        </main>
+    );
 }
