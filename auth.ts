8bc40442d9fbff2ab23108d0517d2c355504ae2b
Danil Mikhaylov <damikh13@gmail.com>
2024-12-07 19:26:26 +0300

    updated DB schema (and brought drizzle adapter) so google authorized users are now saved to our DB.

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { database } from "./app/db/database";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-})
+    adapter: DrizzleAdapter(database),
+    providers: [Google],
+});
