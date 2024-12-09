a19cc72124f88a626d8d61c2710f4d353762349f
Danil Mikhaylov <damikh13@gmail.com>
2024-12-09 17:31:20 +0300

    signout button redirects to main page to avoid errors

--- components/signout-button.tsx
+++ components/signout-button.tsx
@@ -1,15 +1,17 @@
-import { signOut } from "@/auth"
-import { Button } from "@/components/ui/button"
- 
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+
 export function SignOut() {
-  return (
-    <form
-      action={async () => {
-        "use server"
-        await signOut()
-      }}
-    >
-      <Button type="submit">Sign Out</Button>
-    </form>
-  )
+    return (
+        <form
+            action={async () => {
+                "use server";
+                await signOut({
+                    redirectTo: "/",
+                });
+            }}
+        >
+            <Button type="submit">Sign Out</Button>
+        </form>
+    );
 }
